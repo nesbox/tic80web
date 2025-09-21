75523aef86767b4e84c0e168ac0ee703bc01ae4a
Vadim Grigoruk <grigoruk@gmail.com>
2025-09-21 20:11:22 +0200

    learn page wiki link

--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -17,30 +17,34 @@ const Learn = () => {
   }, []);
 
   return (
-    <div className="container mt-4 learn-content">
-      <ReactMarkdown
-        remarkPlugins={[remarkSlug as any]}
-        rehypePlugins={[[rehypeAutolinkHeadings as any, {
-          behavior: 'wrap'
-        }]]}
-        components={{
-          code: ({ node, className, children, ...props }: any) => {
-            return (
-              <code
-                className={className}
-                style={{
-                  color: '#e83e8c',
-                }}
-                {...props}
-              >
-                {children}
-              </code>
-            );
-          }
-        }}
-      >
-        {helpContent}
-      </ReactMarkdown>
+    <div>
+      <h2 className="text-center">You can also learn about TIC-80 on the <a href="https://github.com/nesbox/TIC-80/wiki">Github Wiki</a>.</h2>
+      <hr/>
+      <div className="container mt-4 learn-content">
+        <ReactMarkdown
+          remarkPlugins={[remarkSlug as any]}
+          rehypePlugins={[[rehypeAutolinkHeadings as any, {
+            behavior: 'wrap'
+          }]]}
+          components={{
+            code: ({ node, className, children, ...props }: any) => {
+              return (
+                <code
+                  className={className}
+                  style={{
+                    color: '#e83e8c',
+                  }}
+                  {...props}
+                >
+                  {children}
+                </code>
+              );
+            }
+          }}
+        >
+          {helpContent}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 };
