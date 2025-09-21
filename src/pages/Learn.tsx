22ef90810f2965f26241ba9ce79b39a00197904d
Vadim Grigoruk <grigoruk@gmail.com>
2025-09-21 19:31:54 +0200

    learn page

--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -1,9 +1,47 @@
 
+import { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkSlug from 'remark-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import helpContent from '../data/help.md?raw';
+
 const Learn = () => {
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const element = document.getElementById(hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, []);
+
   return (
-    <>
-      Learn
-    </>
+    <div className="container mt-4">
+      <ReactMarkdown
+        remarkPlugins={[remarkSlug as any]}
+        rehypePlugins={[[rehypeAutolinkHeadings as any, {
+          behavior: 'wrap'
+        }]]}
+        components={{
+          code: ({ node, className, children, ...props }: any) => {
+            return (
+              <code
+                className={className}
+                style={{
+                  color: '#e83e8c',
+                }}
+                {...props}
+              >
+                {children}
+              </code>
+            );
+          }
+        }}
+      >
+        {helpContent}
+      </ReactMarkdown>
+    </div>
   );
 };
 
